3e6c313ce078e52fb365e7c1b1db42bbed22c39a
elmalfoy73 <elmalfoy73@gmail.com>
2019-04-20 18:37:16 +0300

    Bottom navigation

--- app/src/main/java/com/example/elizabeth/timetable/MainActivity.java
+++ app/src/main/java/com/example/elizabeth/timetable/MainActivity.java
@@ -1,13 +1,55 @@
-package com.example.elizabeth.timetable;
+package bottomnav.hitherejoe.com.bottomnavigationsample;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textTimetable;
+    private TextView textAssigment;
+    private TextView textSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textTimetable = (TextView) findViewById(R.id.text_timetable);
+        textAssigment = (TextView) findViewById(R.id.text_schedules);
+        textSetting = (TextView) findViewById(R.id.text_music);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                                textTimetable.setVisibility(View.VISIBLE);
+                                textAssigment.setVisibility(View.GONE);
+                                textSetting.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_schedules:
+                                textTimetable.setVisibility(View.GONE);
+                                textAssigment.setVisibility(View.VISIBLE);
+                                textSetting.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_music:
+                                textTimetable.setVisibility(View.GONE);
+                                textAssigment.setVisibility(View.GONE);
+                                textSetting.setVisibility(View.VISIBLE);
+                                break;
+                        }
+                        return false;
+                    }
+                });
     }
 }
+
